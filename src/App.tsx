90f8be34b09c0cb57d7b33791cc477f01456c843
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-08-29 02:38:45 +0700

    new feature: indicate cheapest item

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,24 @@ import cx from "classnames";
 import { ReactNode, useState } from "react";
 
 export default function App() {
+  const [itemPrices, setItemPrices] = useState([0, 0, 0, 0, 0]);
+
+  const cheapestIndex = itemPrices.reduce<{ price: number; index: number } | undefined>(
+    (cheapest, price, index) =>
+      price === 0
+        ? cheapest
+        : cheapest == null
+        ? { price, index }
+        : price < cheapest.price
+        ? { price, index }
+        : cheapest,
+    undefined,
+  )?.index;
+
+  function updatePrice(index: number, value: number) {
+    setItemPrices(itemPrices.map((v, i) => (i === index ? value : v)));
+  }
+
   return (
     <Container>
       <div className={cx("text-[50px]")}>Cheapest!</div>
@@ -9,7 +27,7 @@ export default function App() {
       <div
         className={cx(
           "grid",
-          "grid-cols-5",
+          "grid-cols-6",
           "gap-[50px]",
 
           "place-items-center",
@@ -23,9 +41,19 @@ export default function App() {
 
         <Header>Price / Item</Header>
 
+        <Header>Cheapest!</Header>
+
         <Header>Actions</Header>
 
-        <Item />
+        <Item isCheapest={cheapestIndex === 0} onChange={(price) => updatePrice(0, price)} />
+
+        <Item isCheapest={cheapestIndex === 1} onChange={(price) => updatePrice(1, price)} />
+
+        <Item isCheapest={cheapestIndex === 2} onChange={(price) => updatePrice(2, price)} />
+
+        <Item isCheapest={cheapestIndex === 3} onChange={(price) => updatePrice(3, price)} />
+
+        <Item isCheapest={cheapestIndex === 4} onChange={(price) => updatePrice(4, price)} />
       </div>
     </Container>
   );
@@ -58,7 +86,13 @@ function Header({ children }: { children: ReactNode }) {
   return <div className={cx("font-bold")}>{children}</div>;
 }
 
-function Item() {
+function Item({
+  isCheapest,
+  onChange,
+}: {
+  isCheapest: boolean;
+  onChange: (itemPrice: number) => void;
+}) {
   const [itemCount, setItemCount] = useState(1);
   const [totalPrice, setTotalPrice] = useState(0);
   const [itemPrice, setItemPrice] = useState(0);
@@ -76,8 +110,12 @@ function Item() {
           value={itemCount}
           onChange={(e) => {
             const itemCount = e.target.valueAsNumber;
+            const itemPrice = totalPrice / itemCount;
+
             setItemCount(itemCount);
-            setItemPrice(totalPrice / itemCount);
+            setItemPrice(itemPrice);
+
+            onChange(itemPrice);
           }}
         />
       </div>
@@ -89,8 +127,12 @@ function Item() {
           value={totalPrice}
           onChange={(e) => {
             const totalPrice = e.target.valueAsNumber;
+            const itemPrice = totalPrice / itemCount;
+
             setTotalPrice(totalPrice);
-            setItemPrice(totalPrice / itemCount);
+            setItemPrice(itemPrice);
+
+            onChange(itemPrice);
           }}
         />
 
@@ -104,13 +146,19 @@ function Item() {
           value={itemPrice}
           onChange={(e) => {
             const itemPrice = e.target.valueAsNumber;
+            const totalPrice = itemPrice * itemCount;
+
             setItemPrice(itemPrice);
-            setTotalPrice(itemPrice * itemCount);
+            setTotalPrice(totalPrice);
+
+            onChange(itemPrice);
           }}
         />
 
         <div>THB</div>
       </div>
+
+      <div>{isCheapest && "X"}</div>
 
       <button>Remove</button>
     </>
