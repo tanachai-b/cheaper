10dae3186642345f0e39c8908a59982cafc7bf5f
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-08-30 21:36:29 +0700

    new feature: dynamic header

--- src/components/layout-components.tsx
+++ src/components/layout-components.tsx
@@ -1,11 +1,12 @@
 import cx from "classnames";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 export function Container({ children }: { children: ReactNode }) {
   return (
     <div
       className={cx(
-        "size-full",
+        "min-w-full",
+        "min-h-full",
 
         "flex",
         "flex-col",
@@ -20,20 +21,52 @@ export function Container({ children }: { children: ReactNode }) {
 }
 
 export function Header() {
+  const [scale, setScale] = useState(0);
+
+  useEffect(() => {
+    onScroll();
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  function onScroll() {
+    setScale(1 - Math.min(window.scrollY / (300 - 50), 1));
+  }
+
   return (
     <div
       className={cx(
-        "py-[100px]",
-        "bg-[#00a080]",
+        "flex-none",
+        "h-[300px]",
 
-        "grid",
-        "place-items-center",
+        "sticky",
+        "top-0",
 
-        "text-[50px]",
-        "text-[#ffffff]",
+        "invisible",
       )}
     >
-      Cheapest!
+      <div
+        className={cx(
+          "visible",
+
+          "bg-[#00a080]",
+
+          "grid",
+          "place-content-center",
+
+          "text-[#ffffff]",
+          "tracking-[0.25ch]",
+          "font-bold",
+
+          "overflow-clip",
+        )}
+        style={{
+          height: `${50 + (300 - 50) * scale}px`,
+          fontSize: `${20 + (40 - 20) * scale}px`,
+        }}
+      >
+        CHEAPEST!
+      </div>
     </div>
   );
 }
